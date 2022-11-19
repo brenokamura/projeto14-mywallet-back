7f5e5c4d361f99b196a6ee5b209434969089f2e1
brenokamura <96897837+brenokamura@users.noreply.github.com>
2022-11-19 00:03:17 -0300

    Update authController.js
    Correções sign-in

--- src/authController.js
+++ src/authController.js
@@ -54,22 +54,28 @@ export { signUp }
 
 async function signIn(req, res){
     const {email, password } = req.body;
-  
-   
-try{
+    
+    const isUser = userSchema.validate({
+        email, password
+    })
+    if(isUser.error){
+        return res.sendStatus(400)
+    }try{
     const user = await db.collection("users").findOne({
         email,
      });
-     if(!user){
-        return res.sendStatus(404)
+    const passwordValid = bcrypt.compareSync(password, user.password);
 
-     }
+    if (!user || !passwordValid) {
+      return res.send(401);
+    }
     const token = uuid()
-    mongoClient.collection("users_registre").insertOne({
+    mongoClient.collection("users").insertOne({
         userId: user._id,
         token,
     })
-    return res.sendStatus(200).send(token)
-    }
+     return res.sendStatus(200).send(token)
+    }catch{
+     return res.sendStatus(500)	
 }
 export { signIn }
